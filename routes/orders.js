d750cf80451c265fac6faddadd4173c8d1bf5e8a
Rahul Thapa <rahulthapa043@gmail.com>
2021-05-04 00:48:49 +0545

    Today's Order Listed

--- routes/orders.js
+++ routes/orders.js
@@ -3,12 +3,25 @@ const router = express.Router();
 const pool = require("../config/database");
 const todayDate = require("../helpers/todaydate");
 
+router.get("/", (req, res) => {
+  pool.query(`SELECT * FROM orders_${todayDate}`, (err, result, fields) => {
+    if (err) res.status(500).send(err);
+    res.send({
+      success: true,
+      data: result,
+    });
+  });
+});
+
 router.get("/:userId", (req, res) => {
   pool.query(
     `SELECT * FROM orders_${todayDate} WHERE user_id = ${req.params.userId}`,
     (err, result, fields) => {
       if (err) res.status(500).send(err);
-      res.send(result);
+      res.send({
+        success: true,
+        data: result,
+      });
     }
   );
 });
@@ -18,7 +31,10 @@ router.get("/:userId/:orderId", (req, res) => {
     `SELECT * FROM orders WHERE id = ${req.params.orderId} AND user_id = ${req.params.userId}`,
     (err, result, fields) => {
       if (err) throw err;
-      res.send(result);
+      res.send({
+        success: true,
+        data: result,
+      });
     }
   );
 });
